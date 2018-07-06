84c1cb894228da2a4000b83d10b30adf8a36589d
Ashton Holgate <ashton.holgate@gmail.com>
2018-07-06 17:24:01 +0100

    Added GA tracking to major events

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as ReactGA from "react-ga";
 require("./Footer.scss");
 
 export interface FooterProps {
@@ -12,7 +13,14 @@ export default class Footer extends React.Component<FooterProps> {
 			<div className="footer">
 				<div className="footer__text-container">
 					<p className="footer__text">designed and developed by</p>
-					<a className="footer__text footer__text--name" href="http://www.ashtonholgate.com">Ashton Holgate</a>
+					{/* <a className="footer__text footer__text--name" href="http://www.ashtonholgate.com">Ashton Holgate</a> */}
+					<ReactGA.OutboundLink
+						className="footer__text footer__text--name"
+						eventLabel="www.ashtonholgate.com"
+						to="http://www.ashtonholgate.com"
+						target="_blank">
+						Ashton Holgate
+					</ReactGA.OutboundLink>
 				</div>
 			</div>
 		);
--- src/containers/simulationsContainer.tsx
+++ src/containers/simulationsContainer.tsx
@@ -1,11 +1,16 @@
 import { WhenWillYouWinStore } from "../redux/store";
 import { Dispatch, connect } from "react-redux";
+import * as ReactGA from "react-ga";
 import Simulations, { SimulationsDispatchProps, SimulationsStoreProps } from "../components/simulations/Simulations";
-import { lottoNumbersChosenSelector, lottoDrawsSelector, lottoKeyFactsSelector, lottoSimulationHistorySelector,
+import {
+	lottoNumbersChosenSelector, lottoDrawsSelector, lottoKeyFactsSelector, lottoSimulationHistorySelector,
 	isLottoSimulationOpenSelector, areLottoChosenNumbersValidSelector, simulationStatusSelector, euromillionsNumbersChosenSelector, euromillionsDrawsSelector,
-		euromillionsKeyFactsSelector, euromillionsSimulationHistorySelector, isEuromillionsSimulationOpenSelector, areEuromillionsChosenNumbersValidSelector } from "../redux/simulationsReducer/selectors";
-import { changeLottoSelectedNumberAction, onUpdateLottoDrawsAction, onUpdateSimulationStatusAction, onLottoOpenAction, changeEuromillionsSelectedNumberAction,
-		onUpdateEuromillionsDrawsAction, onEuromillionsOpenAction } from "../redux/simulationsReducer/actions";
+	euromillionsKeyFactsSelector, euromillionsSimulationHistorySelector, isEuromillionsSimulationOpenSelector, areEuromillionsChosenNumbersValidSelector
+} from "../redux/simulationsReducer/selectors";
+import {
+	changeLottoSelectedNumberAction, onUpdateLottoDrawsAction, onUpdateSimulationStatusAction, onLottoOpenAction, changeEuromillionsSelectedNumberAction,
+	onUpdateEuromillionsDrawsAction, onEuromillionsOpenAction
+} from "../redux/simulationsReducer/actions";
 import { Draw, SimulationStatus } from "../models/Simulation";
 
 const mapStateToProps = (store: WhenWillYouWinStore): SimulationsStoreProps => ({
@@ -25,13 +30,27 @@ const mapStateToProps = (store: WhenWillYouWinStore): SimulationsStoreProps => (
 });
 
 const mapDispatchToProps = (dispatch: Dispatch): SimulationsDispatchProps => ({
-	onLottoSelectedNumbersChange: (value: (number|null)[], areNumbersValid): void => { dispatch(changeLottoSelectedNumberAction({ value, areNumbersValid })); },
+	onLottoSelectedNumbersChange: (value: (number | null)[], areNumbersValid): void => { dispatch(changeLottoSelectedNumberAction({ value, areNumbersValid })); },
 	onUpdateLottoDraws: (draws: Draw[]): void => { dispatch(onUpdateLottoDrawsAction({ draws })); },
 	handleLottoOpenClick: (value: boolean): void => { dispatch(onLottoOpenAction({ value })); },
-	onEuromillionsSelectedNumbersChange: (value: (number|null)[], areNumbersValid): void => { dispatch(changeEuromillionsSelectedNumberAction({ value, areNumbersValid })); },
+	onEuromillionsSelectedNumbersChange: (value: (number | null)[], areNumbersValid): void => { dispatch(changeEuromillionsSelectedNumberAction({ value, areNumbersValid })); },
 	onUpdateEuromillionsDraws: (draws: Draw[]): void => { dispatch(onUpdateEuromillionsDrawsAction({ draws })); },
 	handleEuromillionsOpenClick: (value: boolean): void => { dispatch(onEuromillionsOpenAction({ value })); },
-	onUpdateSimulationStatus: (value: SimulationStatus): void => { dispatch(onUpdateSimulationStatusAction({ value })); }
+	onUpdateSimulationStatus: (value: SimulationStatus): void => {
+		dispatch(onUpdateSimulationStatusAction({ value }));
+		if (value === 1) {
+			ReactGA.event({
+				category: "Simulations",
+				action: "Lotto Simulation"
+			});
+		}
+		if (value === 2) {
+			ReactGA.event({
+				category: "Simulations",
+				action: "Euromillions Simulation"
+			});
+		}
+	}
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Simulations);
